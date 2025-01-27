022d6c6303f923e9eb94519f7e2d140b5db26ad3
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-27 00:59:04 +0000

    logic for all 3 pages

--- app.rb
+++ app.rb
@@ -24,9 +24,31 @@ get("/rock") do
 end
 
 get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
 
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  end
+
+  erb(:paper)
 end
 
 get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
 
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  elsif @comp_move == "scissors"
+    @outcome = "tied"
+  end
+
+  erb(:scissors)
 end
