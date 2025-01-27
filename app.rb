3304b5f4c440e752e78df2eb471d5ca7c92ea783
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-27 00:37:24 +0000

    first save

--- app.rb
+++ app.rb
@@ -7,3 +7,26 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  elsif @comp_move == "scissors"
+    @outcome = "won"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+
+end
+
+get("/scissors") do
+
+end
